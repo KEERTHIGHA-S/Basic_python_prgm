107a26566add7d22cc792e7ad3e2af33a43564f6
KEERTHIGHA-S <159118835+KEERTHIGHA-S@users.noreply.github.com>
2024-06-23 21:11:28 +0530

    strongnumber.py

--- strongnumber.java
+++ strongnumber.java
@@ -1,43 +1,26 @@
-public class strongnumber
-{
-    public static void main(String[] args)
-    {
-        
-        int i=145;
-        int c;
-        int sum=0;
-        int temp;
-        temp=i;
-        while(temp>0)
-        {
-            c=temp%10;
-            temp=temp/10;
-          
-            int fact=1;
-            
-            while(c>=1)
-            {
-                fact=fact*c;
-                
-                c=c-1;
-                
-                
-            }
-            System.out.println("the fact value" + fact);
-            sum=sum+fact;
-        
-            
-        }
-        System.out.println(sum);
-        
-         if(sum==i)
-         {
-             System.out.println("This is strong number");
-         }
-         else
-         {
-             System.out.println("This is not a strong number");
-         }
-    }
+i=145
+temp=i
+c=[]
+while(temp>0):
+    k=temp%10
+    temp=temp//10
+    fact=1
+    sum=0 
    
-}
+    while (k>0):
+        fact=fact*k
+        k=k-1
+    c.append(fact)
+    print(fact)
+print(c)
+sum=0
+for k in c:
+    sum=sum+k
+print(sum)
+
+if(sum==i):
+    print("Strong number")
+else:
+    print("Not a Strong number")
+    
+    
